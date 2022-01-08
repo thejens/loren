2a9bbba08c4c56d4a215433c80239a79c369b94f
Jens Larsson <jens.larsson56@gmail.com>
2022-01-08 19:24:35 +0100

    make conf a class

--- loren/load.py
+++ loren/load.py
@@ -1,31 +1,25 @@
 import os
-
-from importlib import import_module
+try:
+    import json
+    import jsonschema
+except ImportError:
+    pass  
 from collections.abc import Mapping
-from typing import Dict, Any, Type, List
-from functools import cached_property, lru_cache
-from loren.parsers.base_parser import BaseParser
+from typing import Dict, Any, List
+from functools import cached_property
 from loren.utilities.file_reader import LorenFileReader
-from loren.utilities.configuration import get_configuration
+from loren.utilities.configuration import LorenConfiguration
 
 
-@lru_cache()
-def get_parser_class(class_ref) -> Type[BaseParser]:
-    return getattr(
-        import_module(".".join(class_ref.split(".")[:-1])),
-        class_ref.split(".")[-1],
-    )
+def recursive_dict_update(default: Mapping, update: Mapping) -> None:
+    for key, value in update.items():
+        if key not in default:
+            default[key] = value
+        elif isinstance(value, Mapping):
+            recursive_dict_update(default[key], value)
 
 
-def recursive_dict_update(d: dict, u: dict) -> None:
-    for k, v in u.items():
-        if k not in d:
-            d[k] = v
-        elif isinstance(v, Mapping):
-            recursive_dict_update(d[k], v)
-
-
-class NotInitiated(object):
+class NotInitiated():
     def __init__(self, sources):
         self.sources = sources
 
@@ -61,14 +55,14 @@ class LorenDict(dict):
         else:
             root_path = str(root_path)
             if os.path.isfile(root_path):
-                self.config = get_configuration(root_path)
+                self.config = LorenConfiguration(path=root_path)
                 self.sources = [root_path]
                 recursive_dict_update(self, self._file_loader(root_path))
 
             else:
                 self.root_path = root_path.rstrip("/")
                 if config is None:
-                    self.config = get_configuration(root_path)
+                    self.config = LorenConfiguration(path=root_path)
                 else:
                     self.config = config
 
@@ -114,14 +108,11 @@ class LorenDict(dict):
             )
         file_contents = LorenFileReader.read(path)
         for file_extension in reversed(path.strip(".").split(".")[1:]):
-            parser_class_path = self.config["file_handlers"].get(
-                file_extension, self.config["file_handlers"]["*"]
-            )
-            parser_class = get_parser_class(parser_class_path)
+            parser_class = self.config.get_parser_class(file_extension)
             file_contents = parser_class.parse(
                 data=file_contents,
                 file_path=path,
-                root_path=self.config["base_path"],
+                root_path=self.config.base_path,
                 additional_args=self.additional_args,
             )
         return file_contents
@@ -135,9 +126,9 @@ class LorenDict(dict):
 
     @cached_property
     def sources(self) -> Dict[str, List[str]]:
-        source_files = {}
+        source_files: Dict[str, List[str]] = {}
         for item in os.scandir(self.root_path):
-            if self._ignore_func(item):
+            if self.config.is_ignored_file(item):
                 continue
 
             item_name = item.name
@@ -148,16 +139,10 @@ class LorenDict(dict):
             source_files.setdefault(item_name, []).append(item.path)
         return source_files
 
-    def _ignore_func(self, item: os.DirEntry):
-        return self.config["ignore_paths"].match_file(
-            item.path[len(self.config["base_path"]) + 1 :]
-            + ("/" if item.is_dir() else "")
-        )
-
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"{self.__class__.__name__}({super().__repr__()})"
 
-    def get(self, key, default=None):
+    def get(self, key: Any, default: Any=None) -> Any:
         try:
             return self.__getitem__(key)
         except KeyError as e:
@@ -166,16 +151,10 @@ class LorenDict(dict):
             else:
                 raise e
 
-    def __getattribute__(self, name):
-        return super().__getattribute__(name)
-
     def items(self):
         for key in self.keys():
             yield key, self.__getitem__(key)
 
     def validate(self, schema_path: str) -> None:
-        import jsonschema
-        import json
-
         with open(schema_path, "r") as schema:
             jsonschema.validate(self.to_dict(), json.load(schema))
--- tests/test_utilities/test_configuration.py
+++ tests/test_utilities/test_configuration.py
@@ -1,14 +1,13 @@
 import yaml
 from loren.utilities.configuration import (
     DEFAULT_CONFIG,
-    init_configuration,
-    get_configuration,
+    LorenConfiguration
 )
 from importlib import import_module
 from pathspec.pathspec import PathSpec
+from pathlib import PosixPath
 
-
-def test_default_config():
+def test_default_config() -> None:
     assert isinstance(DEFAULT_CONFIG, dict)
     assert "file_handlers" in DEFAULT_CONFIG
     assert isinstance(DEFAULT_CONFIG["file_handlers"], dict)
@@ -20,35 +19,30 @@ def test_default_config():
     assert len(DEFAULT_CONFIG["ignore"]) > 0
 
 
-def test_import_handlers():
+def test_import_handlers() -> None:
     for handler in DEFAULT_CONFIG["file_handlers"].values():
         import_module(".".join(handler.split(".")[:-1]))
 
 
-def test_init_configuration(tmp_path):
-    init_configuration(str(tmp_path))
+def test_init_configuration(tmp_path: PosixPath) -> None:
+    LorenConfiguration(str(tmp_path)).dump_configuration()
     with open(f"{tmp_path}/.loren.yml", "r+") as configuration:
         config = yaml.safe_load(configuration)
     assert config == DEFAULT_CONFIG
 
 
-def test_get_configuration_no_file(tmp_path):
-    conf = get_configuration(str(tmp_path))
-    expected_conf = DEFAULT_CONFIG.copy()
-    expected_conf["base_path"] = str(tmp_path)
-    assert isinstance(conf["ignore_paths"], PathSpec)
-    del conf["ignore_paths"]
-    assert conf == expected_conf
+def test_get_configuration_no_file(tmp_path: PosixPath) -> None:
+    conf = LorenConfiguration(str(tmp_path))
+    assert conf.base_path == str(tmp_path)
+    assert isinstance(conf.ignore_paths, PathSpec)
 
 
-def test_get_configuration_file_exists(tmp_path):
-    with open(f"{tmp_path}/.loren.yml", "w+") as configuration:
+def test_get_configuration_file_exists(tmp_path: PosixPath) -> None:
+    with open(f"{tmp_path}/.loren.yml", "w+", encoding="utf-8") as configuration:
         configuration.write("ignore: ['templates*']")
 
-    conf = get_configuration(str(tmp_path))
+    conf = LorenConfiguration(str(tmp_path))
     expected_conf = DEFAULT_CONFIG.copy()
     expected_conf["base_path"] = str(tmp_path)
     expected_conf["ignore"] = ["templates*"]
-    assert conf["ignore_paths"].match_file("templates/some_template.yml")
-    del conf["ignore_paths"]
-    assert conf == expected_conf
+    assert conf.ignore_paths.match_file("templates/some_template.yml")
